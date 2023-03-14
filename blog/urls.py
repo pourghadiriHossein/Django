82dcd86d3e2baf25509e78cc88f4ecd0fc15df26
pourghadiriHossein <hossein.654321@yahoo.com>
2023-03-14 23:31:07 +0330

    session10

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('', views.post_list_view, name='posts_list'),
     path('<int:pk>/', views.post_detail_view, name='post_detail'),
     path('create/', views.post_create_view, name='create'),
+    path('<int:pk>/update/', views.post_update_view, name='update'),
+    path('<int:pk>/delete/', views.post_delete_view, name='delete'),
 ]
